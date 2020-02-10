cbc57dda6a649efa51445565c36bdc1951bd45c3
orionslg <solang.orion@gmail.com>
2020-02-10 16:34:02 +0700

    Test hello world

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const cool = require('cool-ascii-faces')
 const express = require('express')
-const path = require('path')
+const app = express();
 const PORT = process.env.PORT || 3000
+const cors = require('cors')
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(201).json('Hello World')
+})
+  
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
