d54a8e57035a9c3ce074fc2e61bd4741556d90cc
hendry <hendry_wu1984@yahoo.com>
2020-02-10 19:42:06 +0700

    deploy heroku done

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const UserRoutes = require('./UserRoutes.js');
 
+router.get('/', (req, res) => {
+    res.send('Welcome to Kanban');
+})
 router.use('/users', UserRoutes);
+
 
 module.exports = router;
