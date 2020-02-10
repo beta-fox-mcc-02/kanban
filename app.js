dff8b06fcb85a0498dfda0cb3862967e53af4c83
yufias <yupi41@gmail.com>
2020-02-10 18:12:51 +0700

    app.js11

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-const express = require("express")
-const app = express()
-const PORT = 3000
-app.use(express.json())
-app.use(express.urlencoded({
+const express = require("express");
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.use(
+  express.urlencoded({
     extended: false
-}))
+  })
+);
 
 app.get("/", (req, res, next) => {
-    res.status(200).json({
-        data: "HOME",
-        msg: "HOME PAGE"
-    })
-})
+  res.send("HOME PAGE");
+});
 
 app.listen(PORT, () => {
-    console.log("listening to app", PORT)
-})
+  console.log("listening to app", PORT);
+});
