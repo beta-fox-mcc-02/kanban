53ec028e36da7146839376e596bdfcd027a0bff0
andrumahardi <andrumahardi77@gmail.com>
2020-02-10 17:29:09 +0700

    second commit

--- app.js
+++ app.js
@@ -7,4 +7,8 @@ app.use(cors())
 app.use(express.urlencoded({ extended:false }))
 app.use(express.json())
 
+app.get('/', (req, res, next) => {
+    res.send(`Helloo world`)
+})
+
 app.listen(port, ()=>{})
