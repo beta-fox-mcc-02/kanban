0afc1c0f8f3e193ff530bc67caed0d8a1154edd2
orionslg <solang.orion@gmail.com>
2020-02-10 18:00:29 +0700

    fix user.js models

--- models/user.js
+++ models/user.js
@@ -38,10 +38,7 @@ module.exports = (sequelize, DataTypes) => {
       }
     }
   })
-  const User = sequelize.define('User', {
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {});
+  
   User.associate = function(models) {
     // associations can be defined here
   };
