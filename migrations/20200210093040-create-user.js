057a2ed7743b0564d28131255cba9038c5fd0f9c
hendry <hendry_wu1984@yahoo.com>
2020-02-10 20:18:31 +0700

    test deploy

--- migrations/20200210093040-create-user.js
+++ migrations/20200210093040-create-user.js
@@ -8,30 +8,8 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      email: {
-        type: Sequelize.STRING,
-        isUnique: true,
-        allowNull: {
-          args: false,
-          msg: "Please input the email address"
-        },
-        validate: {
-          isEmail : {
-            args: true,
-            msg: "Please input the right email format"
-          }
-        }
-      },
-      password: {
-        type: Sequelize.STRING,
-        allowNull:false,
-        validate: {
-          len: {
-            args: [6],
-            msg: "Password minimun 6 characters"
-          }
-        }
-      },
+      email: Sequelize.STRING,
+      password: Sequelize.STRING,
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
