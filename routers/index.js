553cedc157243946fe571772b57aa0455e92f188
hikmanisyariful <syarifulfajar017@gmail.com>
2020-02-10 18:48:07 +0700

    router home

--- routers/index.js
+++ routers/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router()
 const userRouter = require('./userRouter')
 
+router.get('/', (req, res) => {
+    res.send('WELCOME TO KANBAN APPS')
+})
 router.use(userRouter)
 
 module.exports = router
