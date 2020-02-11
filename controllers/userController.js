31bf53a2e5f40e953ffc89c9668fb3f66ac7f432
mcc-15 <mcc-15@mcc-15s-Mac-mini.local>
2020-02-11 15:50:28 +0700

    login/register done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const { User } = require('../models')
 const { generateToken } = require('../helpers/jwt')
+const { checkPassword } = require('../helpers/password')
 
 class UserController {
     static register(req, res, next) {
@@ -8,15 +9,50 @@ class UserController {
             first_name, last_name, email, password
         })
             .then(user => {
-                const token = generateToken({
-                    id: user.id,
-                    email: user.email
-                })
                 res.status(201).json({
-                    token
+                    msg: "Register success"
                 })
             })
             .catch(err => {
+                console.log(err)
+                next(err)
+            })
+    }
+
+    static login (req, res, next) {
+        const { email, password } = req.body
+        User.findOne({
+            where: {
+                email
+            }
+        })
+            .then(user => {
+                if (user === null) {
+                    next({
+                        status: 404,
+                        msg: "Email/Password wrong"
+                    })
+                } else {
+                    const check = checkPassword(password, user.password)
+                    if (check) {
+                        const token = generateToken({
+                            id: user.id,
+                            email: user.email,
+                        })
+                        res.status(200).json({
+                            msg: 'login success',
+                            token
+                        })
+                    } else {
+                        next({
+                            status: 404,
+                            msg: "Email/Password wrong"
+                        })
+                    }
+                }
+            })
+            .catch(err => {
+                console.log(err)
                 next(err)
             })
     }
