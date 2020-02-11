5b0cc43374c35fab812cd293e45169247d05a282
yufias <yupi41@gmail.com>
2020-02-11 17:09:29 +0700

    status.json1

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 const userRoute = require("./routes/user")
+const cors = require("cors")
 const PORT = process.env.PORT || 3000;
-app.use(express.json());
 app.use(
   express.urlencoded({
     extended: false
   })
 );
+app.use(express.json());
+app.use(cors())
 
 app.get("/", (req, res, next) => {
   res.status(200).json({
