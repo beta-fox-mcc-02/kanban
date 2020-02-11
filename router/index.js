d898374200acf57fb3d17392ad725a57bcb790f4
Lukman Bayhaqi <lukmanbaihaqi78@gmail.com>
2020-02-11 11:16:44 +0700

    deploy heroku

--- router/index.js
+++ router/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router()
 const user = require('./user')
 
+router.get('/',(req,res)=>{
+    res.send('hello worldie')
+})
 router.use('/', user)
 
 module.exports = router
