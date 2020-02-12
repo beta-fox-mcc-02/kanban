a5ef0550d9b178a784767d9dcbd6c108bca2d0ff
nafies1 <ghuroba.muslim@gmail.com>
2020-02-12 18:48:07 +0700

    handle cr task

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,16 +1,47 @@
-const { Task } = require('../models')
+const { Task, Category, User } = require('../models')
 
 class TaskController {
   static getAllTask (req, res, next) {
-
+    Task.findAll({
+      include: [Category, {
+        model: User,
+        attributes: { 
+          exclude: ['password', 'createdAt', 'updatedAt'] 
+        }
+      }]
+    })
+      .then(task => {
+        console.log(task)
+        res.status(200).json(task)
+      })
+      .catch(next)
   }
 
   static getOneTask (req, res, next) {
-    
+    Task.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(task => {
+        console.log(task)
+        res.status(200).json(task)
+      })
+      .catch(next)
   }
 
   static createTask (req, res, next) {
-    
+    const { title, CategoryId } = req.body
+    Task.create({
+      title,
+      CategoryId,
+      UserId: req.currentUserId
+    })
+      .then(task => {
+        console.log(task)
+        res.status(201).json(task)
+      })
+      .catch(next)
   }
 }
 
