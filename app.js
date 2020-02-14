2ad15681ba11ed78f767ea5c263ebea7981b92ea
baufakhran <baufakhran@gmail.com>
2020-02-14 18:05:00 +0700

    deployed

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const cors = require('cors')
 const router = require('./routes')
 const errorHandler = require('./middlewares/errorHandler')
 
+app.use(cors())
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
-app.use(cors())
 
 app.use('/',router)
 app.use(errorHandler)
