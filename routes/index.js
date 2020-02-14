12635dd0a8b939b2b4e1fabf1da9a6db9388fe28
hendry <hendry_wu1984@yahoo.com>
2020-02-14 20:22:39 +0700

    Testing deploy again

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ const CategoryRoutes = require('./CategoryRoutes.js');
 const TaskRoutes = require('./TaskRoutes.js');
 const UserRoutes = require('./UserRoutes.js');
 
+router.get('/', (req, res) => {
+    res.send('Welcome')
+})
 router.use('/category', CategoryRoutes);
 router.use('/board', TaskRoutes); 
 router.use('/users', UserRoutes);
