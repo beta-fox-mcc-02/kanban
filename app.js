41de5bf3492e279ab52a07e19a330394cb246f90
hendry <hendry_wu1984@yahoo.com>
2020-02-14 21:05:18 +0700

    Trying to deploy morning-journey

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ if(process.env.NODE_ENV === 'development') require("dotenv").config();
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const Routes = require('./routes/index.js');
 const errHandler = require('./middlewares/errHandler.js');
 const cors = require('cors');
