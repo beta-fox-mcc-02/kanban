1624a7399b6afc71ae70358e37b302f3c4096612
GanangPrakoso <ganangmanggolo@gmail.com>
2020-02-14 18:49:40 +0700

    guguru sign in done

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,8 +2,8 @@ const { User } = require('../models')
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs')
 const { Project } = require('../models')
-// const {OAuth2Client} = require('google-auth-library');
-// const client = new OAuth2Client(process.env.CLIENT_ID);
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(process.env.CLIENT_ID);
 
 class UserController {
    static register(req, res, next) {
@@ -47,53 +47,55 @@ class UserController {
          })
    }
 
-   // static gSignIn (req, res, next) {
-   //    let email
-   //    client.verifyIdToken({
-   //       idToken: req.body.token,
-   //       audience: process.env.CLIENT_ID
-   //    })
-   //       .then(data => {
-   //          // console.log(data.payload.email, `heiiiiiiiiiiiiiiiii`);
-   //          email = data.payload.email
+   static gSignIn(req, res, next) {
+      
+      let name
+      let email
+      client.verifyIdToken({
+         idToken: req.body.token,
+         audience: process.env.CLIENT_ID
+      })
+         .then(data => {
+            name = data.payload.name
+            email = data.payload.email
 
-   //          return User.findOne({
-   //             where : {
-   //                email : email
-   //             }
-   //          })
-   //       })
-   //       .then(user => {
-   //          // console.log(user);
+            return User.findOne({
+               where: {
+                  email: email
+               }
+            })
+         })
+         .then(user => {
+            console.log(user);
 
-   //          if (!user) {
-   //             console.log(`user belum terdaftar di database`);               
-   //             let newUser = {
-   //                email,
-   //                password: process.env.SECRET_PASSWORD
-   //             }
+            if (!user) {
+               let newUser = {
+                  name,
+                  email,
+                  password: process.env.SECRET_PASSWORD
+               }
 
-   //             return User.create(newUser)
-   //          }
-   //          else {
-   //             return user
-   //          }
-   //       })
-   //       .then(logUser => {
-   //          // console.log(logUser, `ini masuk then terakhir`);            
-   //          const token = jwt.sign({ id: logUser.id }, process.env.SECRET);
-   //          res.status(200).json({token})
-   //       })
-   //       .catch(err => {
-   //          next(err)
-   //       })
-   // }
+               return User.create(newUser)
+            }
+            else {
+               return user
+            }
+         })
+         .then(logUser => {
+            const token = jwt.sign({ id: logUser.id }, process.env.SECRET);
+            res.status(200).json({ token })
+         })
+         .catch(err => { 
+                       
+            next(err)
+         })
+   }
 
    static getAll(req, res, next) {
       User.findAll()
          .then(data => {
             let results = []
-            for (let i = 0; i<= data.length-1; i++) {
+            for (let i = 0; i <= data.length - 1; i++) {
                let temp = {
                   id: data[i].id,
                   name: data[i].name,
@@ -117,10 +119,10 @@ class UserController {
             model: Project
          }
       })
-         .then(data => {            
+         .then(data => {
             res.status(200).json(data.Projects)
          })
-         .catch (err => {
+         .catch(err => {
             next(err)
          })
    }
--- routes/users.js
+++ routes/users.js
@@ -7,5 +7,6 @@ router.get('/', authentication, userController.getAll)
 router.get('/projects', authentication, userController.getProject)
 router.post('/register', userController.register)
 router.post('/login', userController.login)
+router.post('/gSignIn', userController.gSignIn)
 
 module.exports = router
