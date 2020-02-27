1d8847122f6a221d2d442954b09523cb6337f8d1
mcc-14 <mcc-14@mcc-14s-Mac-mini.local>
2020-02-27 17:53:08 +0700

    deploy attempt

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ const user = require('./userRoutes')
 const task = require('./taskRoutes')
 const category = require('./categoryRoutes')
 
+router.get('/', (req, res) => {
+    res.send('WE ARE CONNECTED')
+})
 router.use('/', user)
 router.use('/tasks', task)
 router.use('/categories', category)
